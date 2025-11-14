f89c25e2863536dd39ebd7b12b63274013eff18a
Dziarur <dziarurrahman@gmail.com>
2025-11-14 22:34:32 +0700

    Update Tebak Angka.py

--- Tebak Angka.py
+++ Tebak Angka.py
@@ -6,12 +6,15 @@ print("Saya mempunyai angka dari 1-30")
 
 while(True):
     jawaban = int(input("Masukkan Tebakan Anda = "))
-    if jawaban == tebak_angka:
-        print("Selamat, jawaban anda benar")
-        break
+    if jawaban > 30:
+        print("Angkanya kan cuma dari 1-30, tebak yang bener dong")
     else:
-        print("Ayo Coba Lagi")
-        pilih = input("Apakah Nyerah (y/n) = ")
-        if pilih == "y":
-            print(f"angkanya adalah = {tebak_angka}")
+        if jawaban == tebak_angka:
+            print("Selamat, jawaban anda benar")
             break
+        else:
+            print("Ayo Coba Lagi")
+            pilih = input("Apakah Nyerah (y/n) = ")
+            if pilih == "y":
+                print(f"Jawabannya adalah = {tebak_angka}")
+                break   
